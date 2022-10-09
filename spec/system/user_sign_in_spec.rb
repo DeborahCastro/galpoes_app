b4531321e86ab7abf952e18fe474d6804ae05830
deh-castro <dehcastrodesign@gmail.com>
2022-10-09 15:56:29 -0300

    Logout e cadastro

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -18,8 +18,30 @@ describe 'Usuário se autentica' do
     expect(page).to have_content 'Login efetuado com sucesso.'
     within('nav') do
      expect(page).not_to have_link 'Entrar'
-     expect(page).to have_link 'Sair'
+     expect(page).to have_button 'Sair'
      expect(page).to have_content 'deh@gmail.com'
     end
+  end
+
+  it 'e faz logout' do
+     # Arrange
+     User.create!(email: 'deh@gmail.com', password: 'password')
+
+     # Act
+     visit root_path
+     click_on 'Entrar'
+       fill_in 'E-mail', with: 'deh@gmail.com'
+       fill_in 'Senha', with: 'password'
+      within('form') do
+        click_on 'Entrar'
+      end
+       click_on 'Sair'
+ 
+     # Assert
+     expect(page).to have_content 'Logout efetuado com sucesso'
+     expect(page).to have_link 'Entrar'
+     expect(page).not_to have_button 'Sair'
+     expect(page).not_to have_content 'deh@gmail.com'
+    
   end
 end
